ac031964b497fa98e04d8e0eec1aa2ca0b5f98ab
IkleelyuneeQ <140011686+IkleelyuneeQ@users.noreply.github.com>
2023-10-16 00:19:39 +0100

    process.env.PORT

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ import bodyParser from "body-parser";
 
 // 2. Create an express app and set the port number.
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ApiKey = "openuv-1oa2crlnnzffmp-io"
 const url = 'https://api.openuv.io/api/v1/uv';
+
+app.set('view engine', 'ejs');
 
 // 3. Use the public folder for static files.
 app.use(express.static("public"));
@@ -63,7 +65,7 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 
-app.set('view engine', 'ejs');
+
 
 
   
